395f020d0c43b59c55aa95cf0b8df983a495c8fc
Baptiste Darthenay <baptiste+bitbucket@darthenay.fr>
2014-10-02 20:43:43 +0200

    Prepared templates for translation.

--- pasportaservo/settings/__init__.py
+++ pasportaservo/settings/__init__.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'django_countries',
     'phonenumber_field',
     'bootstrapform',
     'leaflet',
