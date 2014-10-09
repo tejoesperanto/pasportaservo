f77970a866c4ee5c91ff4517a74b692b6adef2b5
Baptiste Darthenay <baptiste+bitbucket@darthenay.fr>
2014-10-09 22:58:49 +0200

    Drupal migration using Django models.

--- tools/migration.py
+++ tools/migration.py
@@ -1,81 +1,81 @@
 #!/usr/bin/env python
-
-import psycopg2 as pg
-import pymysql as mdb
-from datetime import datetime
-import hashlib
-
+import os, sys
 import pprint
+import pytz
+import pymysql as mdb
+import hashlib
+from datetime import datetime
+
+import django
+from django.utils.timezone import make_aware
+from django.db import transaction
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.insert(0, PROJECT_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'pasportaservo.settings'
+
 
 def u2dt(timestamp):
     """A convenience function to easily convert unix timestamps to datetime strings."""
-    return datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    # return datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    return make_aware(datetime.fromtimestamp(timestamp), pytz.timezone('UTC'))
 
-# Connect to an existing database
-dr = mdb.connect('localhost', 'pasportaservo', 'pasportaservo', 'pasportaservo')
-dj = pg.connect("dbname=pasportaservo user=guillaume")
 
-# First of all, users.
-users = dr.cursor(mdb.cursors.DictCursor)
-users.execute("""
-    SELECT *
-    FROM users u
-    INNER JOIN node n ON n.uid=u.uid AND n.type='profilo'
-    INNER JOIN content_type_profilo p ON p.nid=n.nid
-    WHERE u.uid > 1
-    GROUP BY u.uid
-""")
+@transaction.atomic
+def migrate():
+    # Connect to an existing database
+    dr = mdb.connect('localhost', 'root', 'blfpd', 'pasportaservo')
 
-user = users.fetchone()
-newuser = dj.cursor()
-while user is not None:
-    username = user['name'].strip()
-    if len(username) > 30:
-        if len(user['mail']) > 30:
-            username = user['mail'][0:user['mail'].index('@')]
-            if len(username) > 30:
-                h = hashlib.md5()
-                h.update(username)
-                username = h.digest()
-        else:
-            username = user['mail']
-    try:
-        newuser.execute("""
-            INSERT INTO
-            auth_user(
-                id,
-                password,
-                last_login,
-                is_superuser,
-                username,
-                first_name,
-                last_name,
-                email,
-                is_staff,
-                is_active,
-                date_joined
-            )
-            VALUES(
-                %s, %s, %s, 'f', %s, %s, %s, %s, 'f', 't', %s
-            )""",
-            (user['uid'], user['pass'], u2dt(user['login']), username, user['field_persona_nomo_value'],
-            user['field_familia_nomo_value'], user['mail'], u2dt(user['created'])))
-    except pg.DataError as e:
-        print("Insert error for user {0}".format(user['uid']))
-        print(e)
-        pprint.pprint(user)
-        # Cleanup
-        dj.rollback()
-        newuser.close()
-        users.close()
-        dr.close()
-        exit()
+    # First of all, users.
+    users = dr.cursor(mdb.cursors.DictCursor)
+    users.execute("""
+        SELECT *
+        FROM users u
+        INNER JOIN node n ON n.uid=u.uid AND n.type='profilo'
+        INNER JOIN content_type_profilo p ON p.nid=n.nid
+        WHERE u.uid > 1
+            AND ( (field_lando_value = 'Albanio' AND field_urbo_value is not NULL)
+                OR (field_lando_value <> 'Albanio'))
+        GROUP BY u.uid
+    """)
+
     user = users.fetchone()
 
-dj.commit()
-newuser.close()
-users.close()
-dr.close()
+    from django.contrib.auth.models import User
+    django.setup()
+
+    while user is not None:
+        username = user['name'].strip()
+        if len(username) > 30:
+            if len(user['mail']) > 30:
+                username = user['mail'][0:user['mail'].index('@')]
+                if len(username) > 30:
+                    h = hashlib.md5()
+                    h.update(username)
+                    username = h.digest()
+            else:
+                username = user['mail']
+        newuser = User(
+                id=user['uid'],
+                password=user['pass'],
+                last_login=u2dt(user['login']),
+                is_superuser=False,
+                username=username,
+                first_name=user['field_persona_nomo_value'],
+                last_name=user['field_familia_nomo_value'],
+                email=user['mail'],
+                is_staff=False,
+                is_active=True,
+                date_joined=u2dt(user['created'])
+            )
+        newuser.save()
+        user = users.fetchone()
+
+    users.close()
+    dr.close()
+
+if __name__ == '__main__':
+    migrate()
 
 # Open a cursor to perform database operations
 #cur = dj.cursor()
@@ -97,3 +97,4 @@ dr.close()
 # Close communication with the database
 #cur.close()
 #conn.close()
+
