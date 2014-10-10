16adc2da4fec070586815dbbe893c2f46f874675
Baptiste Darthenay <baptiste+bitbucket@darthenay.fr>
2014-10-10 15:51:13 +0200

    Drupal-Dj migration: impr. Profile, and added Place.

--- tools/migration.py
+++ tools/migration.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os, sys
-import re
 import pprint
 import pytz
 import pymysql as mdb
-import hashlib
-from datetime import datetime, date
+from datetime import datetime
+from getpass import getpass
 
 import django
 from django.utils.timezone import make_aware
 from django.db import transaction
+
+from countries import COUNTRIES
+import getters as g
+
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
 sys.path.insert(0, PROJECT_DIR)
@@ -20,36 +24,11 @@ def u2dt(timestamp):
     """A convenience function to easily convert unix timestamps to datetime TZ aware."""
     return make_aware(datetime.fromtimestamp(timestamp), pytz.timezone('UTC'))
 
-def get_title(sekso):
-    if sekso == 'Ina':
-        return 'Mrs'
-    if sekso == 'Vira':
-        return 'Mr'
-    return ''
-
-
-def get_birth_date(year):
-    if year:
-        year = year.strip(' .-')
-        if len(year) == 4:
-            return date(int(year), 1, 1)
-        if len(year) == 2 and int(year) > 30:
-            return date(int('19'+year), 1, 1)
-        if '/' in year:
-            day, month, y = year.split('/')
-            if len(y) == 2:
-                return date(int('19'+y), int(month), int(day))
-            elif len(y) == 4:
-                return date(int(y), int(month), int(day))
-        else:
-            print('Non valid year:', year)
-    return None
-
-
 @transaction.atomic
 def migrate():
     # Connect to an existing database
-    dr = mdb.connect('localhost', 'root', 'blfpd', 'pasportaservo')
+    passwd = getpass("MySQL password for 'root': ")
+    dr = mdb.connect('localhost', 'root', passwd, 'pasportaservo', charset='utf8')
 
     # First of all, users.
     users = dr.cursor(mdb.cursors.DictCursor)
@@ -58,35 +37,31 @@ def migrate():
         FROM users u
         INNER JOIN node n ON n.uid=u.uid AND n.type='profilo'
         INNER JOIN content_type_profilo p ON p.nid=n.nid
+        LEFT JOIN location_instance
+            ON u.uid = location_instance.uid
+        LEFT JOIN location
+            ON location_instance.lid = location.lid
         WHERE u.uid > 1
             AND ( (field_lando_value = 'Albanio' AND field_urbo_value is not NULL)
                 OR (field_lando_value <> 'Albanio'))
+            AND field_familia_nomo_value <> 12
         GROUP BY u.uid
     """)
 
     user = users.fetchone()
 
     from django.contrib.auth.models import User
-    from hosting.models import Profile
+    from hosting.utils import title_with_particule
+    from hosting.models import Profile, Place
     django.setup()
 
     while user is not None:
-        username = user['name'].strip()
-        if len(username) > 30:
-            if len(user['mail']) > 30:
-                username = user['mail'][0:user['mail'].index('@')]
-                if len(username) > 30:
-                    h = hashlib.md5()
-                    h.update(username)
-                    username = h.digest()
-            else:
-                username = user['mail']
         new_user = User(
                 id=user['uid'],
                 password=user['pass'],
                 last_login=u2dt(user['login']),
                 is_superuser=False,
-                username=username,
+                username=g.get_username(user['name'], user['mail']),
                 email=user['mail'],
                 is_staff=False,
                 is_active=True,
@@ -95,14 +70,31 @@ def migrate():
         new_user.save()
 
         new_profile = Profile(
+                id=user['uid'],
                 user=new_user,
-                title=get_title(user['field_sekso_value']),
-                first_name=user['field_persona_nomo_value'],
-                last_name=user['field_familia_nomo_value'],
-                birth_date=get_birth_date(user['field_naskijaro_value']),
+                title=g.get_title(user['field_sekso_value']),
+                first_name=title_with_particule(user['field_persona_nomo_value']),
+                last_name=title_with_particule(user['field_familia_nomo_value']),
+                birth_date=g.get_birth_date(user['field_naskijaro_value']),
                 description=user['field_pri_mi_value'],
+                avatar=g.get_avatar(user['picture']),
         )
         new_profile.save()
+
+        address = user['field_adreso_value']
+        if address is not None and address.strip():
+            new_place = Place(
+                    address=address.strip(),
+                    description=user['field_detaloj_kaj_rimarkoj_value'].strip(),
+                    city=title_with_particule(user['field_urbo_value'].strip()),
+                    closest_city=title_with_particule(user['field_proksima_granda_urbo_value'].strip()),
+                    postcode=g.get_postcode(user['field_posxtokodo_logxadreso_value']),
+                    country=COUNTRIES.get(user['field_lando_value']),
+                    latitude = user['latitude'],
+                    longitude = user['longitude'],
+            )
+            new_place.save()
+            new_profile.places.add(new_place)
 
         user = users.fetchone()
 
