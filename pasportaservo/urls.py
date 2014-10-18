4c47a1cd2ed4aa1802b79b4666687d63b1d1f79c
Baptiste Darthenay <baptiste+bitbucket@darthenay.fr>
2014-10-18 21:31:34 +0200

    Password change/reset. To be continued...

--- pasportaservo/urls.py
+++ pasportaservo/urls.py
@@ -1,13 +1,24 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import (login, logout,
+    password_change, password_change_done,
+    password_reset, password_reset_done, password_reset_confirm)
 from django.utils.translation import ugettext_lazy as _
 
 
 urlpatterns = patterns('',
-    url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
     url(_(r'^login/$'), login, name='login'),
     url(_(r'^logout/$'), logout, {'next_page':'/'}, name='logout'),
+    url(_(r'^password/$'), password_change, name='password_change'),
+    url(_(r'^password/done/$'), password_change_done  , name='password_change_done'),
+    url(_(r'^password/reset/$'), password_reset, name='password_reset'),
+    url(_(r'^password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$'), 
+        'password_reset_confirm', name='password_reset_confirm'),
+    url(_(r'^password/reset/done/$'), password_reset_done, name='password_reset_done'),
+    # url(_(r'^password/reset/confirm/$'), password_reset_confirm, name='password_reset_confirm'),
+
     url(r'', include('hosting.urls')),
 )
