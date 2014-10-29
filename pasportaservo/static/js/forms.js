f9a451bc388d9abe5fddff7af71e40b79c3a6d78
Baptiste Darthenay <batisteo@users.noreply.github.com>
2014-10-29 12:25:35 +0100

    Improved forms with JS, and some statics moved.

---
+++ pasportaservo/static/js/forms.js
@@ -0,0 +1,10 @@
+$(function() {
+
+    $('#id_birth_date').datepicker({
+        format: "yyyy-mm-dd",
+        weekStart: 1,
+        startView: 2, // 0: month; 1: year; 2: decade;
+        language: "eo"
+    });
+
+});
