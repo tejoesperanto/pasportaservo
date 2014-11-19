6f4dab030b2902c0de350ceb79a7f40710b32ce8
Baptiste Darthenay <baptiste@darthenay.fr>
2014-11-19 11:04:30 +0100

    Migration: remove Profile.places.

---
+++ hosting/migrations/0014_remove_profile_places.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hosting', '0013_place_family_members'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='profile',
+            name='places',
+        ),
+    ]
