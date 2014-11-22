d15ba472b7b1d6b25cc3ca830ecfcc7b9c3a6b7f
Baptiste Darthenay <baptiste@darthenay.fr>
2014-11-22 23:37:50 +0100

    Improved settings/prod.py.

--- pasportaservo/settings/secrets_tpl.py
+++ pasportaservo/settings/secrets_tpl.py
@@ -5,6 +5,7 @@ DJANGO_DB_ENGINE = 'django.db.backends.postgresql_psycopg2'
 DJANGO_DB_NAME = 'pasportaservo'
 DJANGO_DB_USER = ''
 DJANGO_DB_PASSWORD = ''
+DJANGO_DB_HOST = ''
 DJANGO_DB_PORT = '5432'
 
 EMAIL_HOST = ''
