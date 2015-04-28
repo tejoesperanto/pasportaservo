1e79edc5232b98359cdf31d06938030ca9c08b61
Baptiste Darthenay <baptiste@darthenay.fr>
2015-04-28 23:32:51 +0200

    Fixed NoneType has no len() for query string.

--- hosting/views.py
+++ hosting/views.py
@@ -230,7 +230,7 @@ class SearchView(generic.ListView):
                 return location
 
     def get(self, request, *args, **kwargs):
-        self.query = request.GET.get('q')
+        self.query = request.GET.get('q', '')
         if self.query:
             try:
                 geocoder = geopy.geocoders.OpenCage(settings.OPENCAGE_KEY, timeout=5)
