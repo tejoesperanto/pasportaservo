5d7bd2a10451bea02131a1fdb99bc90be639caab
Meir <interDist@users.noreply.github.com>
2016-02-24 16:59:49 +0100

    aldonas informo-bublon pri preferoj de gastiganto (tranokto, ĉiĉeronado, trinkejumado)
    ĝi estas utila por sen-musaj interfacoj (poŝtelefonoj, tabuletoj, ...) en kiuj ne eblas vidigi la titolon de la koncernaj simboloj

--- pasportaservo/static/js/scripts.js
+++ pasportaservo/static/js/scripts.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
     // Kontraŭspamo
-    $("a[href^='mailto:']").each(function() {
-        $(this).attr("href", $(this).attr("href").replace(" [cxe] ", "@"));
+    $('a[href^="mailto:"]').each(function() {
+        $(this).attr('href', $(this).attr('href').replace(" [cxe] ", "@"));
         $(this).html($(this).html().replace(" [cxe] ", "@"));
     });
 
     // Close message
-    $("a.close").click(function(e) {
+    $('a.close').click(function(e) {
         $(this).hide();
-        $(this).parents(".message").slideUp();
+        $(this).parents('.message').slideUp();
         e.preventDefault();
     });
 
     // Lazy load images
-    $(".lazy").addClass("loaded");
+    $('.lazy').addClass('loaded');
 });
