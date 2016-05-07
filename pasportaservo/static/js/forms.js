7e0d7953d863e5c6c6604459c80cb8ebfd90cc84
Meir <interDist@users.noreply.github.com>
2016-05-07 18:42:45 +0200

    aldonas interaktivan indikilon de pasvorta kvalito al registriĝa kaj pasvortŝanĝa formularoj

--- pasportaservo/static/js/forms.js
+++ pasportaservo/static/js/forms.js
@@ -1,3 +1,7 @@
+// @source: https://github.com/tejo-esperanto/pasportaservo/blob/master/pasportaservo/static/js/forms.js
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL v3
+
+
 $(function() {
 
     if (typeof $().datepicker !== "undefined") {
@@ -5,7 +9,7 @@ $(function() {
             format: "yyyy-mm-dd",
             weekStart: 1,
             startView: 2, // 0: month; 1: year; 2: decade;
-            language: "eo"
+            language: document.documentElement.lang
         });
     }
 
@@ -17,7 +21,42 @@ $(function() {
             window.setTimeout(function () { flasher.addClass('out') }, 4000);
         }
     });
-    
+
+    /* password strength meter for password input fields (registration, password change) */
+    if (typeof $().pwstrength !== "undefined") {
+        $.fn.pwstrength.localui = $.fn.pwstrength.localui || {};
+        if (! $.fn.pwstrength.localui.hasOwnProperty(document.documentElement.lang))
+            $.fn.pwstrength.localui[document.documentElement.lang] = [];
+        
+        var pwd_elements = $('#id_password1, #id_new_password1');
+        pwd_elements.pwstrength({
+            common: {
+                zxcvbn: true,
+                userInputs: ['#id_username', '#id_email', '#id_old_password'],
+                zxcvbnTerms: ["pasporta", "servo", "saluton", "esperanto", "esperantist", "zamenhof", "pasvorto", "sekret"]
+            },
+            ui: {
+                showVerdictsInsideProgressBar: true,
+                progressBarMinPercentage: 15,
+                progressBarEmptyPercentage: 0,
+                colorClasses: ['danger', 'danger', 'danger', 'warning', 'success', 'success']
+            }
+        });
+        if (pwd_elements.length > 0) {
+            var options = pwd_elements.data("pwstrength-bootstrap");
+            var t_fallback = options.i18n.t;
+            options.i18n.t = function (key) {
+                var dict = $.fn.pwstrength.localui[document.documentElement.lang];
+                if (key in dict)
+                    return dict[key];
+                return t_fallback(key);
+            };
+            $('#id_username, #id_email').on("input", function () {
+                pwd_elements.pwstrength("forceUpdate");
+            });
+        }
+    }
+
     /* for mass mail form */
     $('#id_body').keyup(function() {
         $('#antauvido').html(marked($(this).val()));
@@ -51,3 +90,6 @@ $(function() {
     }
     
 });
+
+
+// @license-end
---
+++ pasportaservo/static/pwstrength/js/locales/pwstrength-bootstrap.eo.js
@@ -0,0 +1,13 @@
+/**
+ * Esperanto translation for pwstrength-bootstrap
+ */
+(function ($) {
+    $.fn.pwstrength.localui = $.fn.pwstrength.localui || {};
+    $.fn.pwstrength.localui['eo'] = { veryWeak:   "trosimpla",
+                                      weak:       "malforta",
+                                      normal:     "mezforta",
+                                      medium:     "akceptebla",
+                                      strong:     "forta",
+                                      veryStrong: "elstare forta"
+    };
+} (jQuery));
