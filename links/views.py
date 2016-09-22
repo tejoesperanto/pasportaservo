67ca3abdb86887d6a2e4c8f34d674e8e3f591b35
Baptiste Darthenay <baptiste@darthenay.fr>
2016-09-22 19:57:35 +0200

    Funkciantaj unikaj ligiloj.

--- links/views.py
+++ links/views.py
@@ -1,27 +1,72 @@
 from itsdangerous import URLSafeTimedSerializer, BadTimeSignature, SignatureExpired
 
 from django.views import generic
+from django.db import transaction
 from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth import login
+from django.shortcuts import get_object_or_404
+from django.utils.translation import ugettext_lazy as _
+
+from hosting.models import Place
 
 
-class UniqueLink(generic.TemplateView):
-    template_name = 'links/links.html'
-
+class UniqueLinkView(generic.TemplateView):
     def get(self, request, *args, **kwargs):
         self.token = kwargs.pop('token')
         s = URLSafeTimedSerializer(settings.SECRET_KEY, salt='salo')
+        print(self.token)
         try:
-            self.payload = s.loads(self.token, max_age=3)#600*24*60)
+            payload = s.loads(self.token, max_age=3600*24*60, salt='salo')  # 2 months
         except SignatureExpired:
-            self.payload = 'SignatureExpired'
+            self.template_name = 'links/signature_expired.html'
         except BadTimeSignature:
-            self.payload = 'BadTimeSignature'
+            self.template_name = 'links/bad_time_signature.html'
+        else:
+            return self.redirect(request, payload, *args, **kwargs)
         return super().get(request, *args, **kwargs)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def redirect(self, request, payload, *args, **kwargs):
+        try:
+            action = payload['action']
+        except KeyError:
+            self.template_name = 'links/invalid_link.html'
+            return super().get(request, *args, **kwargs)
+        redirect = getattr(self, 'redirect_'+action)(request, payload)
+        return redirect
 
-        context['payload'] = self.payload
-        return context
+    def redirect_confirm(self, request, payload):
+        place = get_object_or_404(Place, pk=payload['place'])
+        if place.confirmed and place.owner.confirmed:
+            return HttpResponseRedirect(reverse('already_confirmed'))
+        with transaction.atomic():
+            place.confirmed = True
+            place.save()
+            place.owner.confirmed = True
+            place.owner.save()
+            place.owner.phones.update(confirmed=True)
+            place.owner.website_set.update(confirmed=True)
+        return HttpResponseRedirect(reverse('confirmed'))
 
-unique_link = UniqueLink.as_view()
+    def redirect_update(self, request, payload):
+        place = get_object_or_404(Place, pk=payload['place'])
+        place.owner.user.backend = settings.AUTHENTICATION_BACKENDS[0]
+        login(request, place.owner.user)
+        messages.success(request, _("You've been automatically logged in. Happy editing!"))
+        return HttpResponseRedirect(place.owner.get_absolute_url())
+
+unique_link = UniqueLinkView.as_view()
+
+
+class ConfirmedView(generic.TemplateView):
+    template_name = 'links/confirmed.html'
+
+confirmed = ConfirmedView.as_view()
+
+
+class AlreadyConfirmedView(generic.TemplateView):
+    template_name = 'links/already_confirmed.html'
+
+already_confirmed = AlreadyConfirmedView.as_view()
