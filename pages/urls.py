f0fbc4829ca9cd43527cd53019b8f91871dc1cd9
Baptiste Darthenay <baptiste@darthenay.fr>
2016-09-25 17:01:10 +0200

    Oftaj demandoj aldonitaj.

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.utils.translation import ugettext_lazy as _
 
-from .views import about, terms_conditions, supervisors
+from .views import about, terms_conditions, supervisors, faq
 
 urlpatterns = [
     url(_(r'^about/$'), about, name='about'),
     url(_(r'^terms-and-conditions/$'), terms_conditions, name='terms_conditions'),
     url(_(r'^supervisors/$'), supervisors, name='supervisors'),
+    url(_(r'^faq/$'), faq, name='faq'),
 ]
