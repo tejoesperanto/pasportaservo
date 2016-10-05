fbcdf88defd55444e35eec266f0839cdaff644f8
Meir <interDist@users.noreply.github.com>
2016-10-05 04:02:37 +0200

    korektetoj

--- hosting/managers.py
+++ hosting/managers.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class NotDeletedManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().exclude(deleted=True)
+        return super(NotDeletedManager, self).get_queryset().exclude(deleted=True)
 
 
 class WithCoordManager(NotDeletedManager):
     def get_queryset(self):
-        return super().get_queryset().filter(
+        return super(WithCoordManager, self).get_queryset().filter(
             latitude__isnull=False,
             longitude__isnull=False,
             available=True,
