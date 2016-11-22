f9cd2a0b75b16e14d35c10e53449f747a414acb3
Baptiste Darthenay <baptiste@darthenay.fr>
2016-11-22 22:14:01 +0100

    Forĵeto de la Drupal la Django importiloj.

--- tools/migration_profileset_to_familymembers.py
+++
@@ -1,6 +0,0 @@
-from hosting.models import Place
-
-for place in Place.objects.all():
-    family_members = place.profile_set.all()
-    place.family_members.add(*family_members)
-    place.profile_set.remove(*family_members)
