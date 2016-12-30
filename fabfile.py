8dc9925df0e9a8c726a0f2870bab852e88d6b69d
Baptiste Darthenay <baptiste@darthenay.fr>
2016-12-30 18:29:44 +0100

    Nova fabfile.

--- fabfile.py
+++ fabfile.py
@@ -5,9 +5,8 @@
 # fab prod deploy
 #
 # Funkcias se vi havas "Host ps" sekcio en via .ssh/config
-# kaj 'tejo' remote al git@github.com:tejo-esperanto/pasportaservo.git
 
-from fabric.api import local, run, sudo, env, prefix, require
+from fabric.api import task, local, run, sudo, env, prefix, require
 
 
 env.hosts = ['ps']
@@ -15,54 +14,61 @@ env.user = 'ps'
 env.use_ssh_config = True
 env.directory = '/srv/%s/pasportaservo'
 env.site = 'staging'  # default
+env.branch = 'master'  # default
 
-
+@task
 def prod():
     env.site = 'prod'
+    env.branch = 'prod'
 
-
+@task
 def staging():
     env.site = 'staging'
+    env.branch = 'master'
 
-
-def push(branch='master', remote='tejo', runlocal=True):
+@task
+def push(remote='origin', branch='master', runlocal=True):
     command = 'git push %s %s' % (remote, branch)
     if runlocal:
         local(command)
     else:
         run(command)
 
-
-def pull(branch='master', remote='tejo', runlocal=True):
+@task
+def pull(remote='origin', branch='master', runlocal=True):
     if runlocal:
         local('git pull --rebase %s %s' % (remote, branch))
     else:
         run('git pull %s %s' % (remote, branch))
 
+@task
+def checkout(remote='origin', branch='master', runlocal=True):
+    command = 'git checkout %s/%s' % (remote, branch)
+    if runlocal:
+        local(command)
+    else:
+        run(command)
 
-def sync(branch='master', remote='origin', runlocal=True):
-    pull(branch, remote, runlocal)
-    push(branch, remote, runlocal)
-
-
+@task
 def requirements():
     run('pip install -Ur requirements.txt')
 
-
+@task
 def collectstatic():
     run("./manage.py collectstatic --noinput")
 
-
+@task
 def migrate():
     run("./manage.py migrate --noinput")
 
-
+@task
 def deploy(branch='devel', remote='origin'):
     require('site', provided_by=[staging, prod])
-    branch = 'master' if env.site == 'prod' else branch
+    require('branch', provided_by=[staging, prod])
 
     with prefix('workon %s' % env.site):
-        pull(branch, remote, False)
+        checkout(remote, env.branch, False)
+        pull(remote, env.branch, False)
         requirements()
         collectstatic()
         migrate()
