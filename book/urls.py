3319e100de3ea824f6d079e68159d0fec2c6fd2f
Meir <interDist@users.noreply.github.com>
2017-06-06 16:04:32 +0200

    auth: book export view

--- book/urls.py
+++ book/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from .views import contact_export
 
 urlpatterns = [
-    url(r'^gastigantoj\.csv$', contact_export, name='contact_export'),
+    url(r'^eksporti/gastigantoj\.csv$', contact_export, name='contact_export'),
 ]
