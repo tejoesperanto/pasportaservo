fc1cf12f39137db278a9328e34bed7da70518119
Meir <interDist@users.noreply.github.com>
2017-10-25 07:18:31 +0200

    assorted fixes: Django 1.11 widget rendering
    rewrote ClearableWithPreviewImageInput and AdminImageWithPreviewWidget

--- hosting/widgets.py
+++ hosting/widgets.py
@@ -1,13 +1,12 @@
+from django.template.loader import get_template
 from django.forms import widgets as form_widgets
 from django.contrib.admin import widgets as admin_widgets
 from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
 
 
 class ClearableWithPreviewImageInput(form_widgets.ClearableFileInput):
-    preview_template = (
-        '<img %(id)s src="%(url)s" data-mfp-always height="42" style="margin: .1em 1em .1em .1em">'
-        '<br class="visible-xxs-inline" />'
-    )
+    preview_template_name = 'hosting/snippets/widget-image_file_preview.html'
 
     class ImagePreviewValue(object):
         def __init__(self, value, template):
@@ -21,24 +20,21 @@ class ClearableWithPreviewImageInput(form_widgets.ClearableFileInput):
         self.field_name = name
         return super().render(name, value, attrs, renderer)
 
-    def get_template_substitution_values(self, value, **kwargs):
-        substitute = super(ClearableWithPreviewImageInput, self).get_template_substitution_values(value)
-        preview_id = 'id="%s-preview_id"' % self.field_name if hasattr(self, 'field_name')  \
-                     else 'name="image-preview"'
-        substitutions = {'url': conditional_escape(value.url), 'id': preview_id}
+    def format_value(self, value, **kwargs):
+        if not self.is_initial(value):
+            return
+        preview_template = get_template(self.preview_template_name)
+        substitutions = {'field_name': getattr(self, 'field_name', None), 'url': value.url}
         substitutions.update(**kwargs)
-        substitute['initial'] = self.preview_template % substitutions
-        return substitute
+        rendered = mark_safe(preview_template.render(substitutions).strip())
+        return self.ImagePreviewValue(value, rendered)
 
 
 class AdminImageWithPreviewWidget(ClearableWithPreviewImageInput, admin_widgets.AdminFileWidget):
-    preview_template = (
-        ClearableWithPreviewImageInput.preview_template +
-        '<span style="margin-right: 1em">%(initial_raw_value)s</span>'
-    )
+    preview_template_name = 'hosting/snippets/widget-image_file_preview_admin.html'
 
-    def get_template_substitution_values(self, value, **kwargs):
-        substitutions = {'initial_raw_value': conditional_escape(value)}
+    def format_value(self, value, **kwargs):
+        substitutions = {'initial_raw_value': value}
         substitutions.update(**kwargs)
-        return super(AdminImageWithPreviewWidget, self).get_template_substitution_values(value, **substitutions)
+        return super().format_value(value, **substitutions)
 
