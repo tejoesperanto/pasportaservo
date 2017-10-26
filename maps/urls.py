d09a83155a263eeedeaefbc812e8063c2a5838ce
Meir <interDist@users.noreply.github.com>
2017-10-26 13:22:27 +0200

    assorted fixes: map point popup javascript (fixes #115)

--- maps/urls.py
+++ maps/urls.py
@@ -4,6 +4,6 @@ from .views import MapStyleView, PublicDataView, WorldMapView
 
 urlpatterns = [
     url(r'^lokoj\.geojson$', PublicDataView.as_view(), name='public_data'),
-    url(r'^(?P<style>\w+)-gl-style\.json$', MapStyleView.as_view(), name='public_data'),
+    url(r'^(?P<style>\w+)-gl-style\.json$', MapStyleView.as_view(), name='world_map_style'),
     url(r'^$', WorldMapView.as_view(), name='world_map'),
 ]
