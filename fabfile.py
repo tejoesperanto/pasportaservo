49d53620ebcd9b575f8bc562d0c16cd529a2d544
Meir <interDist@users.noreply.github.com>
2017-10-30 13:56:51 +0100

    assorted fixes: fabric tasks file

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,12 @@
 # Uzo sur via loka komputilo:
 #
 # pip install fabric3
-# fab deploy  # apriore staging
+# fab deploy  # apriore 'staging'
 # fab prod deploy
 #
-# Funkcias se vi havas "Host ps" sekcio en via .ssh/config
+# Funkcias se vi havas la jenan sekcion en via .ssh/config:
+#     Host ps
+#     HostName 188.166.58.162
 
 from fabric.api import task, local, run, sudo, env, prefix, require
 
@@ -13,22 +15,22 @@ env.hosts = ['ps']
 env.user = 'ps'
 env.use_ssh_config = True
 env.directory = '/srv/%s/pasportaservo'
-env.site = 'staging'  # default
-env.branch = 'devel'  # default
+env.site = 'staging'   # default
+env.branch = 'master'  # default
 
 @task
 def prod():
     env.site = 'prod'
-    env.branch = 'master'
+    env.branch = 'prod'
 
 @task
 def staging():
     env.site = 'staging'
-    env.branch = 'geo'
+    env.branch = 'master'
 
 @task
 def push(remote='origin', branch='master', runlocal=True):
-    command = 'git push %s %s' % (remote, branch)
+    command = "git push %s %s" % (remote, branch)
     if runlocal:
         local(command)
     else:
@@ -37,13 +39,14 @@ def push(remote='origin', branch='master', runlocal=True):
 @task
 def pull(remote='origin', branch='master', runlocal=True):
     if runlocal:
-        local('git pull --rebase %s %s' % (remote, branch))
+        local("git pull --rebase %s %s" % (remote, branch))
     else:
-        run('git pull %s %s' % (remote, branch))
+        run("git checkout -- locale/*/django.mo")
+        run("git pull %s %s" % (remote, branch))
 
 @task
 def checkout(remote='origin', branch='master', runlocal=True):
-    command = 'git checkout %s/%s' % (remote, branch)
+    command = "git checkout %s/%s" % (remote, branch)
     if runlocal:
         local(command)
     else:
@@ -51,12 +54,26 @@ def checkout(remote='origin', branch='master', runlocal=True):
 
 @task
 def requirements():
-    run('pip install -Ur requirements.txt')
+    run("pip install -Ur requirements.txt")
+
+@task
+def updatestrings(runlocal=True, _inside_env=False):
+    command = "./manage.py compilemessages"
+    if not runlocal or runlocal == 'False':
+        if _inside_env:
+            run(command)
+        else:
+            with prefix("workon %s" % env.site):
+                run(command)
+            sudo("supervisorctl restart %s" % env.site)
+    else:
+        local(command)
 
 @task
 def collectstatic():
     run("./manage.py compilescss")
-    run("./manage.py collectstatic --noinput")
+    run("./manage.py collectstatic --noinput %s" %
+        ("--ignore=*.scss" if env.site == 'prod' else ""))
 
 @task
 def migrate():
@@ -67,11 +84,12 @@ def deploy(mode='full', remote='origin'):
     require('site', provided_by=[staging, prod])
     require('branch', provided_by=[staging, prod])
 
-    with prefix('workon %s' % env.site):
+    with prefix("workon %s" % env.site):
         checkout(remote, env.branch, False)
         pull(remote, env.branch, False)
         if mode == 'full':
             requirements()
+            updatestrings(False, _inside_env=True)
             collectstatic()
             migrate()
     if mode != 'html':
