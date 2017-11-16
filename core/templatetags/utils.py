f6f66ba415bf863f88c0da4f1973e4ce4213ebb8
Meir <interDist@users.noreply.github.com>
2017-11-16 23:18:20 +0100

    unuecigas kreadon de gastigo-ankroj laŭ ŝablonoj

---
+++ core/templatetags/utils.py
@@ -0,0 +1,34 @@
+from django import template
+import random
+
+register = template.Library()
+
+
+@register.simple_tag
+def random_identifier(length=None):
+    try:
+        length = int(length)
+    except:
+        length = None
+    if length is None or length <= 0:
+        length = random.randint(16, 48)
+    return ''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789_') for n in range(length))
+
+
+register.simple_tag(func=lambda *args: list(args), name='list')
+
+register.simple_tag(func=lambda **kwargs: dict(kwargs), name='dict')
+
+@register.filter(is_safe=True)
+def are_any(iterable):
+    try:
+        return any(iterable)
+    except (ValueError, TypeError):
+        return bool(iterable)
+
+@register.filter(is_safe=True)
+def are_all(iterable):
+    try:
+        return all(iterable)
+    except (ValueError, TypeError):
+        return bool(iterable)
