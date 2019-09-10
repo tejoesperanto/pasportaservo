7e0c7d4a792459dc65dc2bd65998be1a10004364
Meir <interDist@users.noreply.github.com>
2019-09-11 00:39:18 +0200

    aldonas testojn por modelo Agreement

---
+++ tests/models/test_agreement_model.py
@@ -0,0 +1,29 @@
+import re
+from datetime import datetime
+
+from django.test import override_settings
+
+from django_webtest import WebTest
+
+from ..factories import AgreementFactory
+
+
+class AgreementModelTests(WebTest):
+    def test_field_max_lengths(self):
+        accord = AgreementFactory.build()
+        self.assertEquals(accord._meta.get_field('policy_version').max_length, 50)
+
+    def test_field_blanks(self):
+        accord = AgreementFactory.build()
+        self.assertTrue(accord._meta.get_field('withdrawn').blank)
+
+    def test_field_defaults(self):
+        accord = AgreementFactory.build()
+        self.assertIs(accord._meta.get_field('withdrawn').default, None)
+
+    @override_settings(LANGUAGE_CODE='en')
+    def test_str(self):
+        accord = AgreementFactory()
+        self.assertRegex(str(accord), 'User .+ agreed to \'{}\' '.format(re.escape(accord.policy_version)))
+        accord = AgreementFactory(withdrawn=datetime.now())
+        self.assertRegex(str(accord), 'User .+ agreed to \'{}\' '.format(re.escape(accord.policy_version)))
