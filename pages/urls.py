77f75033aca011d29db25f7a7854c091aa04a09b
Meir <interDist@users.noreply.github.com>
2019-09-19 00:34:41 +0200

    taŭgigas la paĝon Landaj Organizantoj al uzo sen JavaSkripto

--- pages/urls.py
+++ pages/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     url(_(r'^privacy/$'), PrivacyPolicyView.as_view(), name='privacy_policy'),
     url(_(r'^faq/$'), FaqView.as_view(), name='faq'),
     url(_(r'^sv/'), include([
-        url(format_lazy(r'^({book}\:(?P<in_book>1)/)?$', book=pgettext_lazy("URL", 'book')),
+        url(format_lazy(r'^(?:{book}\:(?P<in_book>1)/)?$', book=pgettext_lazy("URL", 'book')),
             SupervisorsView.as_view(), name='supervisors'),
     ])),
     url(_(r'^supervisors/$'), RedirectView.as_view(pattern_name='supervisors')),
