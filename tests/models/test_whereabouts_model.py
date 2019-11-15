25c21b73dc76b8df9c6f3b0adb9f14405306a12a
Meir <interDist@users.noreply.github.com>
2019-11-15 23:22:23 +0100

    korektas testojn por modelo Whereabouts

--- tests/models/test_whereabouts_model.py
+++ tests/models/test_whereabouts_model.py
@@ -1,3 +1,5 @@
+from django.test import override_settings
+
 from django_countries.fields import Country
 from django_webtest import WebTest
 from factory import Faker
@@ -13,11 +15,12 @@ class WhereaboutsModelTests(WebTest):
         self.assertEquals(loc._meta.get_field('state').max_length, 70)
         self.assertEquals(loc._meta.get_field('name').max_length, 255)
 
+    @override_settings(LANGUAGE_CODE='en')
     def test_str(self):
         loc = WhereaboutsFactory.build(name="Córdoba", state="", country=Country('AR'))
-        self.assertEquals(str(loc), "Location of Córdoba (Argentino)")
+        self.assertEquals(str(loc), "Location of Córdoba (Argentina)")
         loc = WhereaboutsFactory.build(name="Córdoba", state="Córdoba", country=Country('AR'))
-        self.assertEquals(str(loc), "Location of Córdoba (Córdoba, Argentino)")
+        self.assertEquals(str(loc), "Location of Córdoba (Córdoba, Argentina)")
         loc = WhereaboutsFactory.build(
             name=Faker('city', locale='el_GR'), state="", country=Country('GR'))
         self.assertEquals(str(loc), "Location of {} ({})".format(loc.name, loc.country.name))
