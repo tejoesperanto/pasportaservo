73cd0b56a4954b58fec1087d3345d53c50a82853
Meir <interDist@users.noreply.github.com>
2020-01-13 00:04:00 +0100

    aldonas testojn por filtro 'split' en modulo 'core'

--- tests/templatetags/test_misc_tags.py
+++ tests/templatetags/test_misc_tags.py
@@ -3,6 +3,7 @@ from collections import namedtuple
 
 from django.template import Context, Template
 from django.test import tag
+from django.utils.html import escape
 
 from django_webtest import WebTest
 from factory import Faker
@@ -15,21 +16,21 @@ class RandomIdentifierTagTests(WebTest):
 
     def test_no_length(self):
         template = Template(self.template_string.substitute(LENGTH=""))
-        page = template.render(Context({}))
+        page = template.render(Context())
         self.assertRegex(page, self.re_default)
 
     def test_invalid_length(self):
         for length in ("aaabbbcc", -6, -12.34, "qwerty1sadf", 0, "view.non_existent_attr"):
             with self.subTest(id_len=length):
                 template = Template(self.template_string.substitute(LENGTH=length))
-                page = template.render(Context({}))
+                page = template.render(Context())
                 self.assertRegex(page, self.re_default)
 
     def test_valid_length(self):
         for length in range(1, 200):
             with self.subTest(id_len=length):
                 template = Template(self.template_string.substitute(LENGTH=length))
-                page = template.render(Context({}))
+                page = template.render(Context())
                 self.assertRegex(page, r'^[A-Za-z1-9_]{%d}$' % length)
 
 
@@ -97,3 +98,79 @@ class CompactFilterTests(WebTest):
             "[Maecenas tempus, tellus eget condimentum rhoncus,"
             " sem quam semper libero, sit amet adipiscing sem neque sed ipsum.]"
         )
+
+    def test_autoescape(self):
+        content = "\nPraesent <nonummy   mi> \"in\fodio\".\r\n\t"
+        template_string = string.Template("""
+            {% load compact from utils %}
+            {% autoescape $SWITCH %}
+                [{% filter compact %}$CONTENT{% endfilter %}]
+                [{% filter compact %}{{ my_var }}{% endfilter %}]/[{{ my_var|compact }}]/
+            {% endautoescape %}
+        """)
+        for switch in ('on', 'off'):
+            with self.subTest(autoescape=switch):
+                template = Template(template_string.substitute(SWITCH=switch, CONTENT=content))
+                page = template.render(Context({'my_var': content}))
+                self.assertEqual(
+                    page.replace(" "*16, "").strip(),
+                    "[Praesent <nonummy mi> \"in odio\".]\n"
+                    + (escape if switch == 'on' else lambda x: x)("[Praesent <nonummy mi> \"in odio\".]/") * 2
+                )
+
+
+@tag('templatetags')
+class SplitFilterTests(WebTest):
+    dummy_object = object()
+    test_data = (
+        ("", [""]),
+        ("xyzq", ["xyzq"]),
+        ("1,2,3", ["1", "2", "3"]),
+        ("a,b,", ["a", "b", ""]),
+        ("a,,c", ["a", "", "c"]),
+        (",,,,", ["", "", "", "", ""]),
+        ("<a>,<b>,</c>", ["<a>", "<b>", "</c>"]),
+        (":'xs',:-&", [":'xs'", ":-&"]),
+        (123, [123]),
+        (False, [False]),
+        (None, [None]),
+        (dummy_object, [dummy_object]),
+    )
+
+    def test_var_input(self, autoescape=True):
+        # Values of type 'str' are expected to be split into a list of strings,
+        # and HTML-encoded on output if autoescape is "on" (output as-is otherwise).
+        # Values of other types are expected to be wrapped in a list as-is.
+        template_string = string.Template("""
+            {% load split from utils %}
+            {% autoescape $SWITCH %}
+                {% for x in my_var|split:',' %}#{{ x }}#{% endfor %}
+            {% endautoescape %}
+        """)
+        for content, expected_value in self.test_data:
+            with self.subTest(value=content):
+                template = Template(template_string.substitute(SWITCH='on' if autoescape else 'off'))
+                page = template.render(Context({'my_var': content}))
+                self.assertEqual(
+                    page.strip(),
+                    "".join("#{}#".format(escape(part) if autoescape else part) for part in expected_value)
+                )
+
+    def test_direct_input(self, autoescape=True):
+        # Values of type 'SafeData' are expected to be split into a list of strings,
+        # and output as-is.
+        template_string = string.Template("""
+            {% load split from utils %}
+            {% autoescape $SWITCH %}
+                {% for x in "$CONTENT"|split:',' %}#{{ x }}#{% endfor %}
+            {% endautoescape %}
+        """)
+        for content, expected_value in self.test_data:
+            with self.subTest(value=content):
+                template = Template(template_string.substitute(SWITCH='on' if autoescape else 'off', CONTENT=content))
+                page = template.render(Context())
+                self.assertEqual(page.strip(), "".join("#{}#".format(part) for part in expected_value))
+
+    def test_nonautoescaped_input(self):
+        self.test_var_input(autoescape=False)
+        self.test_direct_input(autoescape=False)
