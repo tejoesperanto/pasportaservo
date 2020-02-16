14602892cfbc596388d8a18fd9d6cc5d3028a9f3
Meir <interDist@users.noreply.github.com>
2020-02-16 11:09:04 +0100

    korektas la funkciadon de ŝablona etikedo 'domain'

--- core/templatetags/domain.py
+++ core/templatetags/domain.py
@@ -6,23 +6,23 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def domain(context, url='', asvar=None):
+def domain(context, url=''):
     if 'request' in context:
-        protocol = 'https' if context['request'].is_secure else 'http'
+        protocol = 'https' if context['request'].is_secure() else 'http'
         _domain = get_current_site(context['request']).domain
-    elif 'protocol' and 'domain' in context:  # Django emails
+    elif 'protocol' in context and 'domain' in context:
+        # Django emails
         protocol, _domain = context['protocol'], context['domain']
-    elif 'site' in context:  # Postman emails
+    elif 'site' in context:
+        # Postman emails
         _domain = context['site'].domain
         protocol = 'https' if 'pasportaservo.org' in _domain else 'http'
-    else:  # Fallback
+    else:
+        # Fallback
         if settings.DEBUG:
             protocol, _domain = 'http', 'localhost:8000'
         else:
             protocol, _domain = 'https', settings.ALLOWED_HOSTS[0]
 
     link = '{}://{}{}'.format(protocol, _domain, url)
-    if asvar:
-        context[asvar] = link
-        return ''
     return link
