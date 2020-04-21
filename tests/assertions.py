f1af571f7f3883c587c6244a6b8970033566f296
Meir <interDist@users.noreply.github.com>
2020-04-21 22:28:46 +0200

    aldonas testojn por ensaluta formularo UserAuthenticationForm

--- tests/assertions.py
+++ tests/assertions.py
@@ -4,6 +4,13 @@ class AdditionalAsserts:
         """
         Asserts that the string has the given prefix and postfix.
         """
+        self.assertStartsWith(string, prefix, msg)
+        self.assertEndsWith(string, postfix, msg)
+
+    def assertStartsWith(self, string, prefix=None, msg=None):
+        """
+        Asserts that the string has the given prefix.
+        """
         if not isinstance(string, str):
             self.fail("First argument is not a string")
         if prefix and not string.startswith(prefix):
@@ -12,6 +19,13 @@ class AdditionalAsserts:
                 + ("[...]" if len(string) > len(prefix)+10 else string[len(prefix)+10:]),
                 prefix
             )))
+
+    def assertEndsWith(self, string, postfix=None, msg=None):
+        """
+        Asserts that the string has the given postfix.
+        """
+        if not isinstance(string, str):
+            self.fail("First argument is not a string")
         if postfix and not string.endswith(postfix):
             self.fail(self._formatMessage(msg, "'{}' does not end with '{}'".format(
                 ("[...]" if len(string) > len(postfix)+10 else string[:-len(postfix)-8])
