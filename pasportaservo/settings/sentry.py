577c8c71e17ac1fb36f6d7e55377b18182e4524b
Meir <interDist@users.noreply.github.com>
2020-05-15 06:16:55 +0200

    korektas retpoŝtadon al administrantoj

--- pasportaservo/settings/sentry.py
+++ pasportaservo/settings/sentry.py
@@ -6,9 +6,9 @@ from .base import get_env_setting
 
 def sentry_init(env=None):
     sentry_sdk.init(
-        dsn=get_env_setting("SENTRY_DSN"),
+        dsn=get_env_setting('SENTRY_DSN'),
         integrations=[DjangoIntegration()],
-        environment={"PROD": "production", "UAT": "staging"}.get(env, "dev"),
+        environment={'PROD': "production", 'UAT': "staging"}.get(env, "development"),
 
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
