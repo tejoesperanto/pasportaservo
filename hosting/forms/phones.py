ec15ebfcfc2be3e7beb943c4b385f2dd5edba9a8
Meir <interDist@users.noreply.github.com>
2020-07-03 01:40:37 +0200

    korektas la telefonan formularon
    (aldono aŭ ŝanĝo de telefonnumero egala al jam ekzistanta, sed
     forigita, numero reuzu la ekzistantan objekton anstataŭ kraŝi
     pro unikeco-konflikto, ĉar la uzanto ne scias ke la forviŝita
     numero daŭre estas en la datumbazo...)

--- hosting/forms/phones.py
+++ hosting/forms/phones.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 from ..models import Phone
@@ -24,13 +25,40 @@ class PhoneForm(forms.ModelForm):
         cleaned_data = super().clean()
         if 'number' in cleaned_data:
             data = cleaned_data['number'].as_e164
-            phones = Phone.objects.filter(number=data, profile=self.profile)
-            number_list = phones.values_list('number', flat=True)
-            if data in number_list:
-                # Check is done for object creation and for object update.
-                if not self.instance.number or data != self.instance.number.as_e164:
+            if self.instance.number and data == self.instance.number.as_e164:
+                phones = Phone.all_objects.none()
+            else:
+                # Check is done for object creation (self.instance.number == None)
+                # and for object update (self.instance.number != data)
+                phones = Phone.all_objects.filter(number=data, profile=self.profile)
+            assert len(phones) <= 1, "Something is very wrong with the DB."
+            if len(phones) == 1:
+                self.existing_phone = phones[0]
+                if not phones[0].deleted:
                     self.add_error('number', _("You already have this telephone number."))
         return cleaned_data
+
+    def save(self, commit=True):
+        phone = super().save(commit=False)
+        if hasattr(self, 'existing_phone') and self.existing_phone.deleted:
+            # Just overwrite the existing deleted phone object with new data
+            # (i.e., type and comments), because the user did not remember
+            # deleting this phone number.
+            for field in filter(lambda f: f != 'number', self._meta.fields):
+                setattr(self.existing_phone, field, getattr(phone, field))
+            phone = self.existing_phone
+            # Clear the deletion timestamp.
+            phone.deleted_on = None
+            # Clear the verification user id and timestamp.
+            phone.set_check_status(self.profile.user, clear_only=True, commit=False)
+            # Mark the original phone object being updated as deleted.
+            if self.instance.pk:
+                self.instance.deleted_on = timezone.now()
+                self.instance.save(update_fields=['deleted_on'])
+        if commit:
+            phone.save()
+        return phone
+    save.alters_data = True
 
 
 class PhoneCreateForm(PhoneForm):
