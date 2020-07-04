0d8d20b94613f0306dd45daed8d8ff8d501166ab
Meir <interDist@users.noreply.github.com>
2020-07-04 08:54:33 +0200

    kompletigas testojn por gravatar

--- hosting/gravatar.py
+++ hosting/gravatar.py
@@ -7,11 +7,11 @@ def email_to_gravatar(email, fallback='', size=140):
     mail_hash = hashlib.md5()
     mail_hash.update(email.encode('utf-8').lower().strip())
 
-    if fallback != '':
+    if fallback and fallback != '':
         return "https://www.gravatar.com/avatar/{0}?d={1}&s={2}".format(
             mail_hash.hexdigest(),
             django.utils.http.urlquote(fallback),
-            size
+            size or ''
         )
     else:
         return "https://www.gravatar.com/avatar/{0}".format(mail_hash.hexdigest())
