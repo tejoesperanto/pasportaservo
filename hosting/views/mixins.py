7524286295ac3a40537019f69eaee3fe552e6a94
Meir <interDist@users.noreply.github.com>
2021-02-21 23:01:26 +0100

    ebligas al administrantoj konservi informojn sen ilin konfirmi

--- hosting/views/mixins.py
+++ hosting/views/mixins.py
@@ -198,7 +198,7 @@ class UpdateMixin(object):
     def form_valid(self, form):
         self.object.set_check_status(
             self.request.user,
-            clear_only=getattr(self, 'update_partial', False),
+            clear_only=getattr(self, 'update_partial', False) or ('_save-only' in self.request.POST),
             commit=False)
         return super().form_valid(form)
 
