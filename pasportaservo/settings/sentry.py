6af17c559654f09d4d0cf966fc012b00531b4148
Meir <interDist@users.noreply.github.com>
2023-08-15 17:15:42 +0200

    aldonas observadon de la rendimento far Sentry

--- pasportaservo/settings/sentry.py
+++ pasportaservo/settings/sentry.py
@@ -1,7 +1,7 @@
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
-from .base import get_env_setting
+from .base import CURRENT_COMMIT, get_env_setting
 
 
 def sentry_init(env=None):
@@ -9,8 +9,14 @@ def sentry_init(env=None):
         dsn=get_env_setting('SENTRY_DSN'),
         integrations=[DjangoIntegration()],
         environment={'PROD': "production", 'UAT': "staging"}.get(env, "development"),
+        release=CURRENT_COMMIT[0] or None,
 
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
         # send_default_pii=True,
+
+        # Percentage of error events to sample.
+        sample_rate=1.0,
+        # Percentage of transaction events to sample for performance monitoring.
+        traces_sample_rate=0.75,
     )
