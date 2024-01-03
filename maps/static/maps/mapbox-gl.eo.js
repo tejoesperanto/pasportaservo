064ae6366c1c9c8637be3f10ffd6734195acef76
Meir <interDist@users.noreply.github.com>
2024-01-03 21:49:51 +0100

    aldonas mankantajn etikedo-tradukojn por interaga mapo

--- maps/static/maps/mapbox-gl.eo.js
+++ maps/static/maps/mapbox-gl.eo.js
@@ -5,11 +5,13 @@
  window.addEventListener("load", function() {
      mapboxgl.localui = mapboxgl.localui || {};
      mapboxgl.localui['eo'] = {
+         'AttributionControl.ToggleAttribution': "Baskuligi aŭtorrajtan atribuon",
          'FullscreenControl.Enter': "Eniri tutekranan reĝimon",
          'FullscreenControl.Exit': "Forlasi tutekranan reĝimon",
          'GeolocateControl.FindMyLocation': "Trovu mian pozicion",
          'GeolocateControl.LocationNotAvailable': "Pozicio ne haveblas",
          'LogoControl.Title': "Logotipo de Mapbox",
+         'Map.Title': "Interaga mapo",
          'NavigationControl.ResetBearing': "Reorientigi mapon al la nordo",
          'NavigationControl.ZoomIn': "Zomi",
          'NavigationControl.ZoomOut': "Malzomi",
