79eea4a41cac70b26d352a613624509476360991
Meir <interDist@users.noreply.github.com>
2026-03-29 18:45:26 +0200

    vidigas la daton de aprobo por landaj organizantoj

---
+++ core/formats/en/formats.py
@@ -0,0 +1,5 @@
+# The *_FORMAT strings use the Django date format syntax,
+# see https://docs.djangoproject.com/en/stable/ref/templates/builtins/#date
+
+SENTENCE_DATE_FORMAT = r"\o\n js F Y"  # 'on 26th July 1887'
+SENTENCE_DATETIME_FORMAT = r"\o\n js F Y \a\t H:i"  # 'on 26th July 1887 at 18:59'
---
+++ core/formats/eo/formats.py
@@ -0,0 +1,5 @@
+# The *_FORMAT strings use the Django date format syntax,
+# see https://docs.djangoproject.com/en/stable/ref/templates/builtins/#date
+
+SENTENCE_DATE_FORMAT = r"\j\e j\-\a \d\e F Y"  # 'je 26-a de julio 1887'
+SENTENCE_DATETIME_FORMAT = r"j\-\a\n \d\e F Y \j\e H:i"  # '26-an de julio 1887 je 18:59'
