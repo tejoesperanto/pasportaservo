3707b90117cb377bac9ae870e33e1639fd7fccd8
Meir <interDist@users.noreply.github.com>
2026-03-31 18:18:02 +0200

    ebligas kondiĉan eksaktualiĝon de profilaj konfirmoj kaj kontroloj (far LO) (ref 51ca02f)

--- tests/models/test_managers.py
+++ tests/models/test_managers.py
@@ -9,6 +9,7 @@ from django.utils.timezone import make_aware
 
 from factory import Faker
 from factory.django import DjangoModelFactory
+from waffle.testutils import override_switch
 
 from hosting.managers import (
     NotDeletedManager, NotDeletedRawManager, TrackingManager,
@@ -79,7 +80,7 @@ class TrackingManagersTests(TestCase if TYPE_CHECKING else object):
 
         # The `all_objects` manager is expected to fetch all instances of
         # the model, with boolean flag annotations.
-        qs = model.all_objects.order_by('-id')
+        qs = model.all_objects.order_by('id')
         with self.subTest(manager='all_objects'):
             self.assertEqual(len(qs), 5 + existing_items_count['all_objects'])
             for obj in qs:
@@ -88,31 +89,68 @@ class TrackingManagersTests(TestCase if TYPE_CHECKING else object):
                 self.assertTrue(hasattr(obj, 'confirmed'))
                 self.assertTrue(hasattr(obj, 'checked'))
 
-        # Object which was confirmed within the confirmation validity period
-        # is expected to have the 'confirmed' flag set to True.
-        self.assertEqual(qs[3].pk, test_data[1].pk)
-        self.assertTrue(qs[3].confirmed)
-        # Object which was confirmed outside the confirmation validity period
-        # is expected to have the 'confirmed' flag set to False.
-        self.assertEqual(qs[2].pk, test_data[2].pk)
-        self.assertFalse(qs[2].confirmed)
-        # Object which was checked within the confirmation validity period
-        # is expected to have the 'checked' flag set to True.
-        self.assertEqual(qs[1].pk, test_data[3].pk)
-        self.assertTrue(qs[1].checked)
-        # Object which was checked outside the confirmation validity period
-        # is expected to have the 'checked' flag set to True.
-        # (Check exvalidation is turned off at the moment.)
-        self.assertEqual(qs[0].pk, test_data[4].pk)
-        self.assertTrue(qs[0].checked)
+        # For no expiry of the confirmations, any date is expected to result
+        # in the object being marked as confirmed. Otherwise, only the more
+        # recent date is expected to do so.
+        for confirm_expiry in (True, False):
+            with (
+                override_switch('HOSTING_DATA_CONFIRMATION_EXPIRY', confirm_expiry),
+                self.subTest(expires=confirm_expiry)
+            ):
+                qs = model.all_objects.order_by('-id')
+                self.assertEqual(qs[4].pk, test_data[0].pk)
+                self.assertFalse(qs[4].confirmed)
+                # Object which was confirmed within the confirmation validity
+                # period is expected to have the 'confirmed' flag set to True.
+                self.assertEqual(qs[3].pk, test_data[1].pk)
+                self.assertTrue(qs[3].confirmed)
+                # Object which was confirmed outside the confirmation validity
+                # period is expected to have the 'confirmed' flag set to True
+                # if confirmations do not expire, otherwise to False.
+                self.assertEqual(qs[2].pk, test_data[2].pk)
+                self.assertIs(qs[2].confirmed, False if confirm_expiry else True)
 
-    @patch('hosting.managers.SiteConfiguration.get_solo', side_effect=DatabaseError)
-    def test_manager_defaults(self, mock_config):
+        # For no expiry of the verifications, any date is expected to result
+        # in the object being marked as checked. Otherwise, only the more
+        # recent date is expected to do so.
+        for verify_expiry in (True, False):
+            with (
+                override_switch('HOSTING_DATA_VERIFICATION_EXPIRY', verify_expiry),
+                self.subTest(expires=verify_expiry)
+            ):
+                qs = model.all_objects.order_by('-id')
+                self.assertEqual(qs[4].pk, test_data[0].pk)
+                self.assertFalse(qs[4].checked)
+                # Object which was checked within the confirmation validity
+                # period is expected to have the 'checked' flag set to True.
+                self.assertEqual(qs[1].pk, test_data[3].pk)
+                self.assertTrue(qs[1].checked)
+                # Object which was checked outside the confirmation validity
+                # period is expected to have the 'checked' flag set to True
+                # if verifications do not expire, otherwise to False.
+                self.assertEqual(qs[0].pk, test_data[4].pk)
+                self.assertIs(qs[0].checked, False if verify_expiry else True)
+
+    @patch('hosting.managers.get_waffle_switch_model')
+    @patch('hosting.managers.SiteConfiguration')
+    def test_manager_defaults(self, mock_config, mock_switch):
         model: TrackingModel = self.factory._meta.model
         faker = Faker._get_faker()
-        # The default validity period is expected to be 42 weeks.
-        self.factory(confirmed_on=make_aware(faker.date_time_between('-288d', '-293d')))
-        self.factory(confirmed_on=make_aware(faker.date_time_between('-295d', '-300d')))
+        mock_config.get_solo.side_effect = DatabaseError
+        mock_switch.return_value.get.side_effect = DatabaseError
+        # The default validity period is expected to be 42 weeks, with user
+        # confirmations expiring by default and supervisor verifications
+        # not expiring.
+        self.factory(
+            confirmed_on=make_aware(faker.date_time_between('-288d', '-293d')),
+            checked_on=make_aware(faker.date_time_between('-288d', '-293d')),
+        )
+        self.factory(
+            confirmed_on=make_aware(faker.date_time_between('-295d', '-300d')),
+            checked_on=make_aware(faker.date_time_between('-730d', '-735d')),
+        )
         qs = model.all_objects.order_by('-id')
         self.assertFalse(qs[0].confirmed)
+        self.assertTrue(qs[0].checked)
         self.assertTrue(qs[1].confirmed)
+        self.assertTrue(qs[1].checked)
