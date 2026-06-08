1e3fe3002eadb830bbb2089326c52295a4290d84
Meir <interDist@users.noreply.github.com>
2026-06-08 20:37:27 +0200

    aldonas notigojn de datumtipoj al formularoj kaj paĝoj en testoj

--- tests/__init__.py
+++ tests/__init__.py
@@ -13,10 +13,16 @@ def with_type_hint(baseclass: type[T]) -> type[T]:
 if TYPE_CHECKING:
     from django.http import HttpResponse
 
-    from webtest import TestResponse
+    from webtest import TestResponse, forms
 
     class DjangoWebtestResponse(TestResponse, HttpResponse):
-        pass
+
+        forms: dict[int | str, forms.Form]
+        """
+        A dictionary containing all the forms in the page as :class:`~webtest.forms.Form`
+        objects. Indexes are both numerical and by form id (if the form was given an id).
+        """
+
 else:
     from django_webtest import (
         DjangoWebtestResponse as OriginalDjangoWebtestResponse,
